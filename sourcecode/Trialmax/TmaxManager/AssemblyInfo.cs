d8171438d7d8ceea0619c905cc6d62fc588638b2
hussain <mhd_world@hotmail.com>
2014-05-20 18:25:38 +0500

    AssemblyInfo.cs

--- sourcecode/Trialmax/TmaxManager/AssemblyInfo.cs
+++ sourcecode/Trialmax/TmaxManager/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("7.0.33.04034")]
+[assembly: AssemblyVersion("7.0.34.05204")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
