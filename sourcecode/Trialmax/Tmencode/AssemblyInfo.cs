ae68faac4b2ddb9ce7718076914c2e1b7b38dcf3
Muhammad Hussain <mhd_world@hotmail.com>
2014-07-23 09:43:54 +0500

    Changed Version for Release 7.0.38

--- sourcecode/Trialmax/Tmencode/AssemblyInfo.cs
+++ sourcecode/Trialmax/Tmencode/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 
 //[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.0.3.07114")]
+[assembly: AssemblyVersion("7.0.4.07234")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
