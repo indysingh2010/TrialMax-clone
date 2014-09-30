4a5b8d446070deef6315fd5ff87fa41c9ae21599
AmynVirani <amyn.karim@tenpearls.com>
2014-09-30 10:33:50 +0500

    Class for Ghostscript PDF BW Conversions

---
+++ sourcecode/Trialmax/Tmdata/GsPdfManager.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FTI.Shared;
+using FTI.Shared.Win32;
+using FTI.Shared.Trialmax;
+using FTI.Trialmax.Forms;
+
+using Ghostscript.NET;
+using Ghostscript.NET.Processor;
+
+namespace FTI.Trialmax.Database
+{
+    public class CTmaxGsPdfManager
+    {
+        #region Constants
+
+        #endregion Constants
+
+        #region Private Members
+
+        /// <summary>Local path where to output files</summary>
+        private string m_outputPath = string.Empty;
+
+        /// <summary>Local path where the document is present</summary>
+        private string m_documentNameWithPath = string.Empty;
+
+        /// <summary>Indicate if auto detect color, grayscale or colored for output files /// </summary>
+        private TmaxPDFOutputType m_outputMode;
+
+        /// <summary>Number of threads to be used. If set to 0, auto manage</summary>
+        private short m_totalThreads;
+
+        /// <summary>Reference to the Ghostscript.dll to use</summary>
+        private GhostscriptVersionInfo m_gvi = null;
+
+        /// <summary>List of switches to be passed for GS Processing</summary>
+        private List<string> m_switches = null;
+
+        /// <summary>Resolution for Output images</summary>
+        private short m_resolution;
+
+        /// <summary>List containing errors if occured</summary>
+        private List<string> m_conversionErrors = null;
+
+        #endregion Private Members
+
+        #region Public Members
+
+        public event EventHandler notifyPDFManager;
+
+        #endregion Public Members
+
+        #region Public Methods
+
+        ///<summary>Constructor</summary>
+        public CTmaxGsPdfManager(string docPath, string outPath, TmaxPDFOutputType outMode = TmaxPDFOutputType.Autodetect, short outResolution = 0, short totThreads = 0)
+        {
+            m_documentNameWithPath = docPath;
+            m_outputPath = outPath;
+            m_outputMode = outMode;
+            m_totalThreads = totThreads;
+            m_resolution = outResolution;
+            m_switches = new List<string>();
+            InitializeGhostScript();
+            AddRequiredSwitches();
+        }
+
+        ///<summary>Start the conversion process using Ghostscript</summary>
+        public bool Process()
+        {
+            using (GhostscriptProcessor processor = new GhostscriptProcessor(m_gvi, true))
+            {
+                processor.Processing += new GhostscriptProcessorProcessingEventHandler(processor_Processing);
+                try
+                {
+                    processor.StartProcessing(m_switches.ToArray(), null);
+                }
+                catch (Exception ex)
+                {
+                    if (m_conversionErrors == null)
+                        m_conversionErrors = new List<string>();
+                    m_conversionErrors.Add(ex.ToString());
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        ///<summary>Return the list of errors if occured any</summary>
+        public List<string> getErrorList()
+        {
+            return m_conversionErrors;
+            //Console.WriteLine(e.CurrentPage.ToString() + " / " + e.TotalPages.ToString());
+        }
+
+        ///<summary>Notify parent that a page is completed so that the status bar can be updated</summary>
+        
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        ///<summary>Initializes the Ghostscript dll that will be used</summary>
+        private bool InitializeGhostScript()
+        {
+            //m_gvi = GhostscriptVersionInfo.GetLastInstalledVersion();
+            m_gvi = new GhostscriptVersionInfo("PDFManager/gsdll32.dll");
+            return !(m_gvi == null);
+        }
+
+        ///<summary>Add all required switch for output</summary>
+        private void AddRequiredSwitches()
+        {
+            AddDefaultSwitches();
+            AddColorSwitch();
+            AddResolutionSwitch();
+            AddOutputSwitch();
+            AddPdfOpenSwitch();
+            //AddErrorLogSwitch();
+        }
+
+        ///<summary>Add default switch for output</summary>
+        private void AddDefaultSwitches()
+        {
+            m_switches.Add("-dSAFER");
+            m_switches.Add("-dNumRenderingThreads=2");
+            m_switches.Add("-dBATCH");
+            m_switches.Add("-dUseCropBox");
+            m_switches.Add("-dNOPAUSE");
+            m_switches.Add("-sPDFPassword=\"\"");
+        }
+
+        ///<summary>Add color switch for output</summary>
+        private void AddColorSwitch()
+        {
+            if (m_outputMode == TmaxPDFOutputType.ForceBW)
+                m_switches.Add("-sDEVICE=tiffg4");
+            else
+                m_switches.Add("-sDEVICE=png16m");
+        }
+
+        ///<summary>Add resolution switch for output</summary>
+        private void AddResolutionSwitch()
+        {
+            if (m_resolution == 0)
+            {
+                if (m_outputMode == TmaxPDFOutputType.ForceBW)
+                    m_switches.Add("-r300");
+                else
+                    m_switches.Add("-r200");
+                return;
+            }
+            m_switches.Add("-r" + m_resolution);
+        }
+
+        ///<summary>Add output target switch</summary>
+        private void AddOutputSwitch()
+        {
+            m_switches.Add(@"-sOutputFile=" + m_outputPath + "\\%04d.tif");
+        }
+
+        ///<summary>Add PDF open switch</summary>
+        private void AddPdfOpenSwitch()
+        {
+            m_switches.Add(m_documentNameWithPath);
+        }
+
+        ///<summary>Add error logging switch</summary>
+        private void AddErrorLogSwitch()
+        {
+            m_switches.Add(">> \"" + m_outputPath + "\\conv.tmp");
+            m_switches.Add("2>> \"" + m_outputPath + "\\conv.err");
+        }
+
+        ///<summary>Progress of the current task if performing any</summary>
+        private void processor_Processing(object sender, GhostscriptProcessorProcessingEventArgs e)
+        {
+            if (notifyPDFManager != null)
+                notifyPDFManager(sender, e);
+            //Console.WriteLine(e.CurrentPage.ToString() + " / " + e.TotalPages.ToString());
+        }
+
+        #endregion Private Methods
+
+        #region Properties
+
+        #endregion Properties
+
+    }
+}
