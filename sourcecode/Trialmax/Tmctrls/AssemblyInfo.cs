38f60417010e1a79f0bbfafee5dea02522ac2605
Amyn Karim <amyn.karim@tenpearls.com>
2016-07-18 09:53:40 +0500

    Version Update for Final Release

--- sourcecode/Trialmax/Tmctrls/AssemblyInfo.cs
+++ sourcecode/Trialmax/Tmctrls/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("7.0.7.02073")]
+[assembly: AssemblyVersion("7.0.8.07156")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
