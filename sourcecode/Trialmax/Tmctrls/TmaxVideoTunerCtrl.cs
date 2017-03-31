acb63a31501ee963a5e82f5eb9b906bf30d0781b
Hussain Ali Akbar <hussain.akbar@tenpearls.com>
2017-03-31 13:49:10 +0500

    revert initial waveform generation technique

--- sourcecode/Trialmax/Tmctrls/TmaxVideoTunerCtrl.cs
+++ sourcecode/Trialmax/Tmctrls/TmaxVideoTunerCtrl.cs
@@ -9,9 +9,6 @@ using System.Diagnostics;
 using FTI.Shared;
 using FTI.Shared.Xml;
 using FTI.Trialmax.ActiveX;
-using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
-using System.Collections.Generic;
 
 namespace FTI.Trialmax.Controls
 {
@@ -23,41 +20,11 @@ namespace FTI.Trialmax.Controls
         /// <summary>Required designer variable</summary>
         private System.ComponentModel.Container components = null;
 
-        /// <summary>The object for accessing the video player</summary>
-        private CTmaxVideoPlayerCtrl m_ctrlPlayer;
-
-        /// <summary>Panel used to group Tune Bar and the wave form picture box</summary>
-        private Panel m_ctrlPanel;
-
-        /// <summary>The picture box that will show the wave form</summary>
-        private PictureBox m_picWave;
-
-        /// <summary>The bitmap object which will store the orignal wave form for the loaded file</summary>
-        private Bitmap m_orignalWave;
-
-        /// <summary></summary>
-        private WaveOut playbackDevice = new WaveOut() { DesiredLatency = 100, NumberOfBuffers = 2 };
-
-        /// <summary>Location of the converter to be used for video to audio conversion</summary>
-        private const string m_converter = "//ffmpeg.exe";
-
-        /// <summary>Format for the converted audio</summary>
-        private const string m_convertToFormat = "wav";
-
         /// <summary>Custom tune bar control for managing tune states</summary>
         private FTI.Trialmax.Controls.CTmaxVideoTuneBarCtrl m_ctrlTuneBar;
 
-        /// <summary></summary>
-        private class MinMax
-        {
-            public float minValue { get; set; }
-            public float maxValue { get; set; }
-            public MinMax(float min, float max)
-            {
-                minValue = min;
-                maxValue = max;
-            }
-        }// public class MinMax
+        /// <summary>Custom video player for playing TrialMax designations</summary>
+        private FTI.Trialmax.Controls.CTmaxVideoPlayerCtrl m_ctrlPlayer;
 
         #endregion Private Members
 
@@ -94,16 +61,6 @@ namespace FTI.Trialmax.Controls
         {
             //	Propagate the events
             BubbleTmaxVideoCtrlEvent(sender, e);
-
-            switch (e.EventId)
-            {
-                case TmaxVideoCtrlEvents.PlayerPositionChanged:
-                    UpdateLocation(e.Position);
-                    break;
-                default:
-                    break;
-
-            }
 
         }// protected void OnTmaxVideoCtrlEvent(object sender, CTmaxVideoCtrlEventArgs e)
 
@@ -262,10 +219,7 @@ namespace FTI.Trialmax.Controls
             Debug.Assert(m_ctrlTuneBar.IsDisposed == false);
 
             if (m_ctrlPlayer.SetProperties(strFileSpec, xmlDesignation) == true)
-            {
                 m_strFileSpec = m_ctrlPlayer.FileSpec;
-                GenerateAudioWave();
-            }
             else
                 bSuccessful = false;
 
@@ -277,145 +231,6 @@ namespace FTI.Trialmax.Controls
             return bSuccessful;
 
         }// public override bool SetProperties(string strFileSpec, CXmlDesignation xmlDesignation)
-
-        /// <summary>This method is called to generate the audio wave form</summary>
-        private void GenerateAudioWave()
-        {
-            // Check if a valid video file is assigned to the player
-            if (string.IsNullOrEmpty(m_ctrlPlayer.FileSpec)) return;
-
-            // Setup FFMPEG location
-            string ffmpeg = System.IO.Directory.GetCurrentDirectory() + m_converter;
-
-            // Verify that ffmpeg exists
-            if (!System.IO.File.Exists(ffmpeg)) return;
-
-            if (System.IO.File.Exists(System.IO.Path.ChangeExtension(m_ctrlPlayer.FileSpec, m_convertToFormat)))
-                System.IO.File.Delete(System.IO.Path.ChangeExtension(m_ctrlPlayer.FileSpec, m_convertToFormat));
-
-            // Execute Video to Audio conversion
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.CreateNoWindow = true;
-            startInfo.UseShellExecute = false;
-            startInfo.RedirectStandardOutput = true;
-            startInfo.RedirectStandardError = true;
-            startInfo.FileName = ffmpeg;
-            startInfo.Arguments = " -y -i ";
-            startInfo.Arguments += "\"" + m_ctrlPlayer.FileSpec + "\"";
-            startInfo.Arguments += " -ss " + m_ctrlPlayer.StartPosition;
-            startInfo.Arguments += " -to " + m_ctrlPlayer.StopPosition;
-            startInfo.Arguments += " \"" + System.IO.Path.ChangeExtension(m_ctrlPlayer.FileSpec, m_convertToFormat) + "\"";
-            process.StartInfo = startInfo;
-
-            Cursor.Current = Cursors.WaitCursor;
-
-            process.Start();
-            process.WaitForExit();
-
-            m_orignalWave = CreateAudioWave(System.IO.Path.ChangeExtension(m_ctrlPlayer.FileSpec, m_convertToFormat));
-            System.IO.File.Delete(System.IO.Path.ChangeExtension(m_ctrlPlayer.FileSpec, m_convertToFormat));
-            m_picWave.Image = (Bitmap)m_orignalWave.Clone();
-
-            Cursor.Current = Cursors.Default;
-        }// private void GenerateAudioWave()
-
-        /// <summary>Generate the wave from the audio file.</summary>
-        /// <param name="fileName">Location of the audio file for which the wave form is to be generated</param>
-        /// <returns></returns>
-        public Bitmap CreateAudioWave(string fileName)
-        {
-            if (!System.IO.File.Exists(fileName)) return null;
-
-            var fileStream = new WaveFileReader(fileName);
-
-            var fileProvider = new WaveFileReader(fileName);
-            playbackDevice.Init(fileProvider);
-
-            var sampleProvider = fileStream.ToSampleProvider();
-            var Values = GetWaveFormValues(sampleProvider, fileStream.WaveFormat, fileStream.Length, fileStream.WaveFormat.SampleRate / 10);
-
-            // Generating the wave form image
-            Bitmap bim = new Bitmap(Values.Count, 200);
-            System.Drawing.Graphics g = Graphics.FromImage(bim);
-
-            Pen pen = new Pen(Color.Red, 2.0f);
-            g.Clear(Color.Black);
-
-            var mid = 100;
-            var yScale = 100;
-
-            for (int i = 0; i < Values.Count; i++)
-            {
-                float Y1 = mid + (Values[i] * yScale);
-                float Y2 = mid - (Values[i] * yScale);
-
-                g.DrawLine(pen, i, Y1, i, Y2);
-            }
-            fileStream.Close();
-            fileProvider.Close();
-            return bim;
-        }// public Bitmap CreateAudioWave(string fileName)
-
-        /// <summary>
-        /// This function is called to generate the Y-values for the waveform
-        /// Source: https://naudio.codeplex.com/discussions/649903
-        /// </summary>
-        /// <param name="provider"></param>
-        /// <param name="format"></param>
-        /// <param name="length"></param>
-        /// <param name="notificationCount"></param>
-        /// <returns></returns>
-        private List<float> GetWaveFormValues(ISampleProvider provider, WaveFormat format, long length, int notificationCount)
-        {
-            int bufferSize = format.ConvertLatencyToByteSize((((WaveOut)playbackDevice).DesiredLatency + ((WaveOut)playbackDevice).NumberOfBuffers - 1) / ((WaveOut)playbackDevice).NumberOfBuffers);
-            var buf = new float[bufferSize];
-            int samplesRead = 0;
-            int count = 0;
-
-            List<float> Values = new List<float>((int)(length / notificationCount));
-
-            float maxValue = 0;
-            while ((samplesRead = provider.Read(buf, 0, buf.Length)) > 0)
-            {
-                for (int n = 0; n < samplesRead; n += format.Channels)
-                {
-                    maxValue = Math.Max(maxValue, buf[n]);
-                    count++;
-                    if (count >= notificationCount && notificationCount > 0)
-                    {
-                        Values.Add(maxValue);
-                        maxValue = count = 0;
-                    }
-                }
-            }
-            return Values;
-        }// private List<MinMax> GetWaveForm
-
-        /// <summary>
-        /// This function is called to draw a vertical bar on the waveform to show the current position of the video
-        /// </summary>
-        /// <param name="position">The time at which the video is.</param>
-        public void UpdateLocation(double position)
-        {
-            if (m_orignalWave == null) return;
-            double length = m_ctrlPlayer.StopPosition - m_ctrlPlayer.StartPosition;
-            position = position - m_ctrlPlayer.StartPosition;
-
-            Bitmap bmp = (Bitmap)m_orignalWave.Clone();
-            Pen blackPen = new Pen(Color.Blue, m_orignalWave.Width / 10000);
-
-            float x1 = (float)(position / length * m_orignalWave.Width);
-            float y1 = 0;
-            float x2 = x1;
-            float y2 = 200;
-            // Draw line to screen.
-            using (var graphics = Graphics.FromImage(bmp))
-            {
-                graphics.DrawLine(blackPen, x1, y1, x2, y2);
-            }
-            m_picWave.Image = bmp;
-        }// public void UpdateLocation(double position)
 
         /// <summary>This method is called to set the control properties</summary>
         /// <param name="xmlLink">The link used to set property values</param>
@@ -604,80 +419,49 @@ namespace FTI.Trialmax.Controls
         /// </summary>
         protected new void InitializeComponent()
         {
-            this.m_ctrlPlayer = new FTI.Trialmax.Controls.CTmaxVideoPlayerCtrl();
-            this.m_ctrlPanel = new System.Windows.Forms.Panel();
             this.m_ctrlTuneBar = new FTI.Trialmax.Controls.CTmaxVideoTuneBarCtrl();
-            this.m_picWave = new System.Windows.Forms.PictureBox();
-            this.m_ctrlPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.m_picWave)).BeginInit();
+            this.m_ctrlPlayer = new FTI.Trialmax.Controls.CTmaxVideoPlayerCtrl();
             this.SuspendLayout();
+            // 
+            // m_ctrlTuneBar
+            // 
+            this.m_ctrlTuneBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.m_ctrlTuneBar.LinkPosition = -1;
+            this.m_ctrlTuneBar.Location = new System.Drawing.Point(0, 248);
+            this.m_ctrlTuneBar.Name = "m_ctrlTuneBar";
+            this.m_ctrlTuneBar.PlayerPosition = -1;
+            this.m_ctrlTuneBar.PreviewPeriod = 1.5;
+            this.m_ctrlTuneBar.Size = new System.Drawing.Size(300, 72);
+            this.m_ctrlTuneBar.StartPosition = -1;
+            this.m_ctrlTuneBar.StopPosition = -1;
+            this.m_ctrlTuneBar.TabIndex = 0;
+            this.m_ctrlTuneBar.TuneMode = FTI.Trialmax.Controls.TmaxVideoCtrlTuneModes.None;
             // 
             // m_ctrlPlayer
             // 
             this.m_ctrlPlayer.AllowApply = true;
             this.m_ctrlPlayer.BackColor = System.Drawing.SystemColors.Control;
-            this.m_ctrlPlayer.ClassicLinks = true;
             this.m_ctrlPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.m_ctrlPlayer.EnableSimulation = false;
-            this.m_ctrlPlayer.LinkPosition = -1D;
+            this.m_ctrlPlayer.LinkPosition = -1;
             this.m_ctrlPlayer.Location = new System.Drawing.Point(0, 0);
             this.m_ctrlPlayer.Name = "m_ctrlPlayer";
-            this.m_ctrlPlayer.PlayerPosition = -1D;
+            this.m_ctrlPlayer.PlayerPosition = -1;
             this.m_ctrlPlayer.PlayOnLoad = false;
-            this.m_ctrlPlayer.PreviewPeriod = 2D;
+            this.m_ctrlPlayer.PreviewPeriod = 2;
             this.m_ctrlPlayer.ShowPosition = true;
             this.m_ctrlPlayer.ShowTranscript = true;
-            this.m_ctrlPlayer.SimulationText = "";
-            this.m_ctrlPlayer.Size = new System.Drawing.Size(300, 303);
-            this.m_ctrlPlayer.StartPosition = -1D;
-            this.m_ctrlPlayer.StopPosition = -1D;
+            this.m_ctrlPlayer.Size = new System.Drawing.Size(300, 248);
+            this.m_ctrlPlayer.StartPosition = -1;
+            this.m_ctrlPlayer.StopPosition = -1;
             this.m_ctrlPlayer.TabIndex = 1;
             this.m_ctrlPlayer.TuneMode = FTI.Trialmax.Controls.TmaxVideoCtrlTuneModes.None;
-            // 
-            // m_ctrlPanel
-            // 
-            this.m_ctrlPanel.Controls.Add(this.m_ctrlTuneBar);
-            this.m_ctrlPanel.Controls.Add(this.m_picWave);
-            this.m_ctrlPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.m_ctrlPanel.Location = new System.Drawing.Point(0, 303);
-            this.m_ctrlPanel.Name = "m_ctrlPanel";
-            this.m_ctrlPanel.Size = new System.Drawing.Size(300, 135);
-            this.m_ctrlPanel.TabIndex = 2;
-            // 
-            // m_ctrlTuneBar
-            // 
-            this.m_ctrlTuneBar.ClassicLinks = true;
-            this.m_ctrlTuneBar.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.m_ctrlTuneBar.EnableLinks = true;
-            this.m_ctrlTuneBar.LinkPosition = -1D;
-            this.m_ctrlTuneBar.Location = new System.Drawing.Point(0, 0);
-            this.m_ctrlTuneBar.Name = "m_ctrlTuneBar";
-            this.m_ctrlTuneBar.PlayerPosition = -1D;
-            this.m_ctrlTuneBar.PreviewPeriod = 1.5D;
-            this.m_ctrlTuneBar.Size = new System.Drawing.Size(300, 83);
-            this.m_ctrlTuneBar.StartPosition = -1D;
-            this.m_ctrlTuneBar.StopPosition = -1D;
-            this.m_ctrlTuneBar.TabIndex = 0;
-            this.m_ctrlTuneBar.TuneMode = FTI.Trialmax.Controls.TmaxVideoCtrlTuneModes.None;
-            // 
-            // m_picWave
-            // 
-            this.m_picWave.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.m_picWave.Location = new System.Drawing.Point(0, 83);
-            this.m_picWave.Name = "m_picWave";
-            this.m_picWave.Size = new System.Drawing.Size(300, 52);
-            this.m_picWave.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.m_picWave.TabIndex = 0;
-            this.m_picWave.TabStop = false;
             // 
             // CTmaxVideoTunerCtrl
             // 
             this.Controls.Add(this.m_ctrlPlayer);
-            this.Controls.Add(this.m_ctrlPanel);
+            this.Controls.Add(this.m_ctrlTuneBar);
             this.Name = "CTmaxVideoTunerCtrl";
-            this.Size = new System.Drawing.Size(300, 438);
-            this.m_ctrlPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.m_picWave)).EndInit();
+            this.Size = new System.Drawing.Size(300, 320);
             this.ResumeLayout(false);
 
         }
